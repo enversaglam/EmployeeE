852361c6f7c44c3cd84162d4910338e4a7f9d01c
Enver Saglam <enversaglam@gmail.com>
2026-01-15 00:25:47 +0100

    Add project files.

---
+++ EmployeeE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
